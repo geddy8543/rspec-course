ba22ef61ebb435d312038226d6d29ca751f942a1
Gail Eddy <86028046+geddy8543@users.noreply.github.com>
2022-06-30 11:01:53 -0500

    adds predicate method examples for testing

---
+++ spec/predicate_methods_spec.rb
@@ -0,0 +1,29 @@
+# puts 0.zero?   # will return true
+#puts 15.zero?  # will return false
+
+#puts 2.even?
+#puts 3.even?
+#puts 3.odd?
+#puts 12.odd?
+
+#puts [].empty?
+#puts [1].empty?
+
+Rspec.describe 'predicate methods and predicate matchers' do
+    it 'can be tested with Ruby methods' do
+        result = 16 / 2
+        expect(result.even?).to eq(true)
+    end
+
+    # this method is much more elegant; uses rspec matcher
+    it 'can be tested with predicate matchers' do
+        expect(16/2).to be_even
+        expect(15).to be_odd
+        expect(0).to be_zero
+        expect([]).to be_empty
+    end
+
+    describe 0 do
+        it { is_expected.to be_zero }
+    end
+end
