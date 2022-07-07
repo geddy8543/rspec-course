bd7712f81b5abe7c9734a8d316193ff4987d1f9f
Gail Eddy <86028046+geddy8543@users.noreply.github.com>
2022-07-07 11:43:43 -0500

    adds movie ruby code and testing

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -31,10 +31,26 @@ class Movie
             actor.act
             actor.fall_off_ladder
             actor.light_on_fire
+            actor.act
         end
     end
 end
 
-actor = Actor.new('Brad Pitt')
-movie = Movie.new(actor)
-movie.start_shooting
+# actor = Actor.new('Brad Pitt')
+# movie = Movie.new(actor)
+# movie.start_shooting
+
+RSpec.describe Movie do
+    let(:stuntman) { double("Mr. Danger", ready?: true, act: 'Any string', fall_off_ladder: 'Sure! Lets do it!', light_on_fire: true)}
+    subject { described_class.new(stuntman) }
+
+    describe '#start_shooting method' do
+        it 'expects an actor to do 3 actions' do
+            # expect(stuntman).to receive(:light_on_fire).once
+            expect(stuntman).to receive(:fall_off_ladder).exactly(1).times
+            expect(stuntman).to receive(:light_on_fire).at_most(1).times #there is also at_least
+            expect(stuntman).to receive(:act).twice #for more than twice use exactly 
+            subject.start_shooting
+        end
+    end
+end
